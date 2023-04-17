374dfd38aafa19b8ccdedc1bead2e927101d6a36
定义一个function <130282047+ABCDEFG42232434523@users.noreply.github.com>
2023-04-17 10:43:08 +0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -5,3 +5,16 @@ function a() {
         alert("联盟解体啦~~~~~~~~~~~~~~~~~~~");
     }
 }
+
+var text = "恭喜解体\n";
+      function button() {
+            document.getElementById("gxgx").innerHTML=text;
+            text += "恭喜解体\n";
+     }
+     
+function slj() {
+    for (let j = 0; j <= 3; j++) {
+            text += "恭喜解体\n";
+    }  
+    document.getElementById("gxgx").innerHTML=text;  
+}
